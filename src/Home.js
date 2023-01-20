3cdf7eaa414d32671e67dbb0cf7573cc65b87e00
Matt Ruder <matt.j.ruder@gmail.com>
2023-01-20 08:49:36 -0600

    Add basic file structure, bootstrap setup, router setup

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+export default function Home() {
+    return (
+        <div className="home">
+        <h1>This is Home</h1>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Music</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    
+        </div>
+    )
+}
